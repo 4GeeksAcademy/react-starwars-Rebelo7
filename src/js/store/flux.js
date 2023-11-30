6cd1a784a1a4bca12e4ba7b9afc170436a6c8a8d
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2023-11-30 20:25:51 +0000

    add cards

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -25,6 +25,36 @@ const getState = ({ getStore, getActions, setStore }) => {
 				const jsonResponse = await textResponse.json();
 				setStore({...store, starships: jsonResponse.results});
 			},
+			getPeopleDetails: async () => {
+				const store = getStore();
+
+				const newPeopleDetails = await Promise.all(store.people.map(async (person) =>{
+					const textResponse = await fetch(person.url);
+					const jsonResponse = await textResponse.json();
+					return { ...person, details: jsonResponse.result};
+				}));
+				setStore({...store, people: newPeopleDetails});
+			},
+			getPlanetsDetails: async () => {
+				const store = getStore();
+
+				const newPlanetDetails = await Promise.all(store.planets.map(async (planet) =>{
+					const textResponse = await fetch(planet.url);
+					const jsonResponse = await textResponse.json();
+					return { ...planet, details: jsonResponse.result};
+				}));
+				setStore({...store, planets: newPlanetDetails});
+			},
+			getStarshipsDetails: async () => {
+				const store = getStore();
+
+				const newStarshipsDetails = await Promise.all(store.starships.map(async (starship) =>{
+					const textResponse = await fetch(starship.url);
+					const jsonResponse = await textResponse.json();
+					return { ...starship, details: jsonResponse.result};
+				}));
+				setStore({...store, starships: newStarshipsDetails});
+			},
 					
 		},
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -5,27 +5,77 @@ import "../../styles/home.css";
 export const Home = () => {
     
 	const { store, actions } = useContext(Context);
+	
+	const handleInitialData = async () => {
+		await actions.getPeople();
+		await actions.getPlanets();
+		await actions.getStarships();
+	}
 
+	const handleFetchDetails = async () =>{
+		actions.getPeopleDetails();
+		actions.getPlanetsDetails();
+		actions.getStarshipsDetails();
+	}
+	const handleAsynccalls = async () => {
+		await handleInitialData();
+		await handleFetchDetails();
+
+	}
 	useEffect(() => {
-		actions.getPeople();
-		actions.getPlanets();
-		actions .getStarships();
+		handleAsynccalls();
 	}, []);
 	
 	return (
+
+					
 		<div className="text-center mt-5">
-			<h1>STARWARS!</h1>
-			<div>
-				<h2>People: </h2>
-				{store.people.map((person) => <p key={person.uid}>{person.name}</p>)}
-			</div> 
-			<div>
-				<h2>Planets: </h2>
-				{store.planets.map((planet) => <p key={planet.uid}>{planet.name}</p>)}
-			</div> 
-			<div>
-				<h2>Starships: </h2>
-				{store.starships.map((starship) => <p key={starship.uid}>{starship.name}</p>)}
+		
+
+			
+			<h2>People: </h2>
+				<div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+				{store.people.map((person) => (
+					<div key={person.uid}>
+						<div className="card"  style={{width: '18rem;'}}>
+								<img src="https://cdn-m-net.dstv.com/images/WidgetBilboard/2020/07/17/788137/14/1594982049-34_Star_Wars__The_Rise_of_Skywalker_Showpage_Billboard_1600_x_800.jpg" className="card-img-top" alt="..."></img>
+								<div className="card-body">
+									<h5 className="card-title">{person.name}</h5>
+									<p className="card-text">{person.details && person.details.properties && (<p key={person.uid}>Height: {person.details.properties.height}</p>)}</p>
+									<a href="#" className="btn btn-primary">Go somewhere</a>
+								</div>
+						</div>					
+					</div> ))}
+				</div> 
+			
+			<h2>Planets: </h2>
+				<div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+				{store.planets.map((planet) => (
+					<div key={planet.uid}>
+						<div className="card"  style={{width: '18rem;'}}>
+							<img src="https://cdn-m-net.dstv.com/images/WidgetBilboard/2020/07/17/788137/14/1594982049-34_Star_Wars__The_Rise_of_Skywalker_Showpage_Billboard_1600_x_800.jpg" className="card-img-top" alt="..."></img>
+							<div className="card-body">
+								<h5 className="card-title">{planet.name}</h5>	
+								<p className="card-text">{planet.details && planet.details.properties && (<p key={planet.uid}>Diameter: {planet.details.properties.diameter}</p>)}</p>
+								<a href="#" className="btn btn-primary">Go somewhere</a>
+							</div>
+						</div> 
+					</div> ))}
+			
+				</div>
+			<h2>Starships: </h2>
+				<div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+				{store.starships.map((starship) => (
+					<div key={starship.uid}>
+						<div className="card"  style={{width: '18rem;'}}>
+							<img src="https://cdn-m-net.dstv.com/images/WidgetBilboard/2020/07/17/788137/14/1594982049-34_Star_Wars__The_Rise_of_Skywalker_Showpage_Billboard_1600_x_800.jpg" className="card-img-top" alt="..."></img>
+							<div className="card-body">	
+								<h5 className="card-title">{starship.name}</h5>
+								<p className="card-text">{starship.details && starship.details.properties && (<p key={starship.uid}>Model: {starship.details.properties.model}</p>)}</p>
+								<a href="#" className="btn btn-primary">Go somewhere</a>
+							</div>
+						</div>
+					</div> ))}
 			</div> 
 		</div>
 	);
